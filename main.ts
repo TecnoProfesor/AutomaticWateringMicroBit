ae42bb141956c2e13382fae8b38c1268432ec382
TecnoProfesor <65020790+TecnoProfesor@users.noreply.github.com>
2023-04-28 10:21:58 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,29 +12,6 @@ while (true) {
     0,
     1
     )
-    if (input.buttonIsPressed(Button.A)) {
-        if (NivelAgua >= LevelWater) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            basic.showLeds(`
-                # . . . #
-                . # . . .
-                # . # . #
-                . # . # .
-                . . # . #
-                `)
-            basic.pause(1000 * Segundos)
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            continue;
-        }
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        Segundos = Segundos + 1
-        if (Segundos > 9) {
-            Segundos = 1
-        }
-        basic.showNumber(Segundos)
-        continue;
-    }
     Humedad = pins.map(
     pins.analogReadPin(AnalogPin.P2),
     0,
@@ -59,6 +36,27 @@ while (true) {
         } else {
             basic.showIcon(IconNames.Sad)
         }
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        if (NivelAgua >= LevelWater) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.showLeds(`
+                # . . . #
+                . # . . .
+                # . # . #
+                . # . # .
+                . . # . #
+                `)
+            basic.pause(1000 * Segundos)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+        }
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        Segundos = Segundos + 1
+        if (Segundos > 9) {
+            Segundos = 1
+        }
+        basic.showNumber(Segundos)
     }
     basic.pause(1000)
 }
